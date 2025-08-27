e5936029f8b21ccb48dd51a91208d49c40e69661
ya7ya-s <150932710+ya7ya-s@users.noreply.github.com>
2025-08-27 11:57:42 +0300

    Add files via upload

---
+++ Project/data/get_pet_labels.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
+#                                                                             
+# PROGRAMMER: yahya azzam
+# DATE CREATED: 24/8/2025                                 
+# REVISED DATE: 
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+#          the image's filename. This function inputs: 
+#           - The Image Folder as image_dir within get_pet_labels function and 
+#             as in_arg.dir for the function call within the main function. 
+#          This function creates and returns the results dictionary as results_dic
+#          within get_pet_labels function and as results within main. 
+#          The results_dic dictionary has a 'key' that's the image filename and
+#          a 'value' that's a list. This list will contain the following item
+#          at index 0 : pet image label (string).
+#
+##
+# Imports python modules
+from os import listdir
+
+# TODO 2: Define get_pet_labels function below please be certain to replace None
+#       in the return statement with results_dic dictionary that you create 
+#       with this function
+# 
+def format_labels(file_name):
+  """
+  function formatting the filenames to appropriate labels
+  """
+  low_case=file_name.lower()
+  split_name=low_case.split("_")
+
+  label=""
+
+  for word in split_name:
+    if word.isalpha():
+      label += (word + ' ')
+  label=label.strip()
+  return label
+
+def get_pet_labels(image_dir):
+  files_list=listdir(image_dir)
+
+
+  """
+    Creates a dictionary of pet labels (results_dic) based upon the filenames 
+    of the image files. These pet image labels are used to check the accuracy 
+    of the labels that are returned by the classifier function, since the 
+    filenames of the images contain the true identity of the pet in the image.
+    Be sure to format the pet labels so that they are in all lower case letters
+    and with leading and trailing whitespace characters stripped from them.
+    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+    Parameters:
+     image_dir - The (full) path to the folder of images that are to be
+                 classified by the classifier function (string)
+    Returns:
+      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      List. The list contains for following item:
+         index 0 = pet image label (string)
+    """
+  
+  result_dic={}
+
+  for i in files_list:
+    if i not in result_dic:
+      label=format_labels(i)
+      result_dic[i]=label
+    # Replace None with the results_dic dictionary that you created with this
+    # function
+  return result_dic
